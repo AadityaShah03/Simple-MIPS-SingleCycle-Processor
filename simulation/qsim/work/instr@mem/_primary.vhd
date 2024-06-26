809f99d5f14b0e0df6e39690e5d8f843393d945a
Aaditya Shah <aaditya.shah@hotmail.com>
2024-06-26 18:53:39 -0400

    fixed clock cycle issues with data memory and instruction memory

--- simulation/qsim/work/instr@mem/_primary.vhd
+++ simulation/qsim/work/instr@mem/_primary.vhd
@@ -3,7 +3,6 @@ use verilog.vl_types.all;
 entity instrMem is
     port(
         address         : in     vl_logic_vector(7 downto 0);
-        clken           : in     vl_logic;
         clock           : in     vl_logic;
         data            : in     vl_logic_vector(31 downto 0);
         wren            : in     vl_logic;
